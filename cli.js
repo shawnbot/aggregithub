36cab5f75954a48a61091fb1e6f59f4f190329c2
Shawn Allen <shawn.allen@gsa.gov>
2015-03-18 17:42:31 -0700

    s/--token/--auth, s/-g/--include, add --exclude, --verbose

--- cli.js
+++ cli.js
@@ -1,14 +1,20 @@
 #!/usr/bin/env node
 var async = require('async'),
-    agg = require('./index'),
+    agg = require('./'),
     yargs = require('yargs')
       .usage('$0 [options] <repos> <aggregate type> [aggregate options]')
       .demand(2)
-      .describe('g', 'only match repo names with this glob-like pattern')
-      .describe('token', 'your GitHub API access token (default: process.env.GITHUB_AUTH_TOKEN)')
+      .describe('auth', 'your GitHub API access token (default: process.env.GITHUB_AUTH_TOKEN) or credentials in the form "username:password"')
+      .describe('include', 'only include repo names with this glob-like pattern')
+        .alias('include', 'i')
+      .describe('exclude', 'exclude repos matching this glob-like pattern')
+        .alias('exclude', 'e')
+      .describe('parallel', 'do this many API requests in parallel')
+        .default('parallel', 10)
+        .alias('parallel', 'p')
       .alias('h', 'help'),
     options = yargs.argv,
-    auth = options.token || process.env.GITHUB_AUTH_TOKEN,
+    auth = options.auth || process.env.GITHUB_AUTH_TOKEN,
     args = options._,
     repos = args[0],
     type = args[1],
@@ -28,11 +34,27 @@ main(function(error, data) {
 });
 
 function main(done) {
+  var match;
+  if (match = auth.match(/^(\w+):(.+)$/)) {
+    auth = {
+      username: match[1],
+      password: match[2]
+    };
+  }
+
   var client = agg.client(auth),
       pattern = options.g;
+
+  client.debug = options.verbose;
+
   async.waterfall([
-    client.matchRepos(repos, pattern),
-    client.aggregate(type, aggArgs)
+    client.matchRepos(repos, {
+      include: options.glob,
+      exclude: options.exclude
+    }),
+    client.aggregate(type, {
+      parallel: options.parallel
+    }, aggArgs)
   ], done);
 }
 
